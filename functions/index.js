ae742fb486f89d8c4d535e246f6f55c6da659842
Chetan77verma <prem.premsharma77@gmail.com>
2020-08-28 14:12:50 +0530

    user registration

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,27 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const app = require('express')();
 var serviceAccount = require("./social-app-9bee3-firebase-adminsdk-ckdj6-362488a62c.json");
 
+//this is required to run function on localhost:5000
 admin.initializeApp({
    credential: admin.credential.cert(serviceAccount),
    databaseURL: "https://social-app-9bee3.firebaseio.com"
 });
 
-const express = require('express');
-const app = express();
+const db = admin.firestore()
+
+
+//this config is required for client library "firebase"
+var firebaseConfig = require("./firebase-client-app-config.json");
+
+//it is client library but we use it sign-in and signup user and get auth token
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
 
 app.get('/screams', (req, res) => { 
-   admin.firestore().collection('screams').orderBy('createAt','desc').get().then((data) => {
+   db.collection('screams').orderBy('createAt','desc').get().then((data) => {
       let screams = [];
       data.forEach(doc => {
          screams.push({
@@ -30,9 +39,7 @@ app.post('/scream',(req, res) => {
       userHandle: req.body.userHandle,
       createAt: new Date().toISOString()
    }
-   admin
-      .firestore()
-      .collection('screams')
+   db.collection('screams')
       .add(newScream)
       .then(doc => {
          return res.status(200).json({ message: `Scream ${doc.id} created successfully !` })
@@ -42,5 +49,47 @@ app.post('/scream',(req, res) => {
          console.error(err)
       })
 });
+
+//signup route
+app.post('/signup', (req, res) => { 
+   const newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      handle: req.body.handle
+   }
+
+   // TODO: validate user
+
+
+   let authToken,userId
+   db.doc(`/users/${newUser.handle}`).get().then(doc => { 
+      if (doc.exists) {
+         return res.status(400).json({handle: "this handle is already taken"})
+      } else { 
+         return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+      }
+   }).then(data => { 
+      // console.log("token",data.user.xa) // we can fetch authtoken this way as well
+      userId = data.user.uid
+      authToken = data.user.xa
+      const userCredentials = {
+         handle: newUser.handle,
+         email: newUser.email,
+         userId: userId,
+         createdAt: new Date().toISOString()
+      }
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+   }).then(() =>{
+      return res.status(201).json({authToken})
+   }).catch((err) => {
+      console.error(err)
+      if (err.code === "auth/email-already-in-use") {
+         return res.status(400).json({ email: "Email is already in use" })
+      } else {
+         return res.status(500).json({ error: err.code })
+      }
+   })
+})
 
 exports.api = functions.https.onRequest(app)
